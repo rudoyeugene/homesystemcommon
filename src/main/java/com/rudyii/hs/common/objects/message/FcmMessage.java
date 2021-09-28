2c40b961bb7f3b2b45b385c5b64f3e34e709e4c0
j-a-c-k <Scienide83>
2021-09-28 21:05:29 +0300

    Cleanup

--- src/main/java/com/rudyii/hs/common/objects/message/FcmMessage.java
+++ src/main/java/com/rudyii/hs/common/objects/message/FcmMessage.java
@@ -2,15 +2,16 @@ package com.rudyii.hs.common.objects.message;
 
 import com.rudyii.hs.common.type.MessageType;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Data
-@SuperBuilder
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class MessageBase {
+public class FcmMessage {
     private long publishedAt;
     private String serverAlias;
     private String serverKey;
